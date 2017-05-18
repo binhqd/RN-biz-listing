4b5d870248ecada158ace2f8d6013333e7585fbd
binhqd <binhqd@gmail.com>
2017-05-19 01:37:43 +0700

    Apply flexbox layout and show list of business

--- app/scenes/Home/index.js
+++ app/scenes/Home/index.js
@@ -4,19 +4,23 @@ import {
   StyleSheet,
   Text,
   View,
-  Button
+  Button,
+  ListView,
+  TouchableHighlight,
+  Image
 } from 'react-native';
 
 class Home extends React.Component {
   static navigationOptions = ({ navigation }) => {
     const {state, setParams} = navigation;
     return {
-      title: 'Dashboard',
+      title: 'Doanh Nghiệp TP Đà Nẵng',
       headerRight: (
         <Icon
+          raised
           name='bars'
           type='font-awesome'
-          color='#f50'
+          color='#000'
           onPress={() => state.params.handleMenuToggle()}
         />
 
@@ -29,8 +33,18 @@ class Home extends React.Component {
     this.state = {
       isOpen: false
     }
-    this.toggleSideMenu = this.toggleSideMenu.bind(this)
+    this.toggleSideMenu = this.toggleSideMenu.bind(this);
+
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(this._genRows({})),
+    };
+
+    this.THUMB_URLS = ['https://fbcdn-dragon-a.akamaihd.net/hphotos-ak-ash3/t39.1997/p128x128/851549_767334479959628_274486868_n.png', 'https://fbcdn-dragon-a.akamaihd.net/hphotos-ak-prn1/t39.1997/p128x128/851561_767334496626293_1958532586_n.png', 'https://fbcdn-dragon-a.akamaihd.net/hphotos-ak-ash3/t39.1997/p128x128/851579_767334503292959_179092627_n.png', 'https://fbcdn-dragon-a.akamaihd.net/hphotos-ak-prn1/t39.1997/p128x128/851589_767334513292958_1747022277_n.png', 'https://fbcdn-dragon-a.akamaihd.net/hphotos-ak-prn1/t39.1997/p128x128/851563_767334559959620_1193692107_n.png', 'https://fbcdn-dragon-a.akamaihd.net/hphotos-ak-prn1/t39.1997/p128x128/851593_767334566626286_1953955109_n.png', 'https://fbcdn-dragon-a.akamaihd.net/hphotos-ak-prn1/t39.1997/p128x128/851591_767334523292957_797560749_n.png', 'https://fbcdn-dragon-a.akamaihd.net/hphotos-ak-prn1/t39.1997/p128x128/851567_767334529959623_843148472_n.png', 'https://fbcdn-dragon-a.akamaihd.net/hphotos-ak-prn1/t39.1997/p128x128/851548_767334489959627_794462220_n.png', 'https://fbcdn-dragon-a.akamaihd.net/hphotos-ak-prn1/t39.1997/p128x128/851575_767334539959622_441598241_n.png', 'https://fbcdn-dragon-a.akamaihd.net/hphotos-ak-ash3/t39.1997/p128x128/851573_767334549959621_534583464_n.png', 'https://fbcdn-dragon-a.akamaihd.net/hphotos-ak-prn1/t39.1997/p128x128/851583_767334573292952_1519550680_n.png'];
   }
+
+  _pressData = ({}: {[key: number]: boolean})
+
 
   componentWillMount() {
     this.props.navigation.setParams({ handleMenuToggle: this.toggleSideMenu.bind(this) });
@@ -48,6 +62,49 @@ class Home extends React.Component {
     })
   }
 
+  hashCode(str) {
+    var hash = 15;
+    for (var ii = str.length - 1; ii >= 0; ii--) {
+      hash = ((hash << 5) - hash) + str.charCodeAt(ii);
+    }
+    return hash;
+  };
+
+  _renderRow(rowData: string, sectionID: number, rowID: number) {
+      var rowHash = Math.abs(this.hashCode(rowData));
+      var imgSource = {
+        uri: this.THUMB_URLS[rowHash % this.THUMB_URLS.length],
+      };
+      return (
+        <TouchableHighlight onPress={() => this._pressRow(rowID)} underlayColor='rgba(0,0,0,0)'>
+          <View>
+            <View style={styles.row}>
+              <Image style={styles.thumb} source={imgSource} />
+              <Text style={styles.text}>
+                {rowData}
+              </Text>
+            </View>
+          </View>
+        </TouchableHighlight>
+      );
+    }
+
+    _genRows(pressData: {[key: number]: boolean}): Array<string> {
+      var dataBlob = [];
+      for (var ii = 0; ii < 100; ii++) {
+        var pressedText = pressData[ii] ? ' (X)' : '';
+        dataBlob.push('Cell ' + ii + pressedText);
+      }
+      return dataBlob;
+    }
+
+    _pressRow(rowID: number) {
+      this._pressData[rowID] = !this._pressData[rowID];
+      this.setState({dataSource: this.state.dataSource.cloneWithRows(
+        this._genRows(this._pressData)
+      )});
+    }
+
   render() {
 
     const MenuComponent = (
@@ -62,19 +119,56 @@ class Home extends React.Component {
             isOpen={this.state.isOpen}
             onChange={this.onSideMenuChange.bind(this)}
             menu={MenuComponent}>
-          <View>
-            <SearchBar
-
-              placeholder='Type Here...' />
-            <Text>Hello, This is Homepage!</Text>
-            <Button
-              onPress={() => navigate('List', { user: 'Lucy' })}
-              title="Chat with Lucy"
-            />
-          </View>
+          <View style={{
+              flex: 1,
+              flexDirection: 'column'
+            }}>
+            <View>
+                <SearchBar placeholder='Type Here...' />
+            </View>
+            <View style={{backgroundColor: 'skyblue'}}>
+                <ListView contentContainerStyle={styles.list}
+                    dataSource={this.state.dataSource}
+                    renderRow={this._renderRow.bind(this)}
+                  />
+                <Button
+                  onPress={() => navigate('List', { user: 'Lucy' })}
+                  title="Chat with Lucy"
+                />
+            </View>
+        </View>
       </SideMenu>
     );
   }
 }
+
+var styles = StyleSheet.create({
+  list: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+  row: {
+    justifyContent: 'center',
+    padding: 5,
+    margin: 10,
+    width: 100,
+    height: 100,
+    backgroundColor: '#F6F6F6',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#CCC'
+  },
+  thumb: {
+    width: 64,
+    height: 64
+  },
+  text: {
+    flex: 1,
+    marginTop: 5,
+    fontWeight: 'bold'
+  }
+});
 
 export default Home;
