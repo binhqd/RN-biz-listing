1696874e656ba974db7a0a88411571720f237e47
binhqd <binhqd@gmail.com>
2017-05-23 20:41:57 +0700

    Update esformatter-jsx config

--- app/components/BusinessDetail/BizGMap.js
+++ app/components/BusinessDetail/BizGMap.js
@@ -7,14 +7,12 @@ class BizGMap extends React.Component {
     return (
       <View style={ styles.mapContainer }>
         <MapView style={ styles.map }
-        region={
-            {
-                latitude: 37.78825,
+                 region={ {
+                            latitude: 37.78825,
                             longitude: -122.4324,
                             latitudeDelta: 0.015,
                             longitudeDelta: 0.0121,
-            }
-        } />
+                          } } />
       </View>
       );
   }
