ed9dd20b2f6cae45107b324e088b3b47e83b8edd
binhqd <binhqd@gmail.com>
2017-05-27 01:54:41 +0700

    Move dump data loading code to /dump folder

--- app/components/SideMenu/MenuComponent.js
+++ app/components/SideMenu/MenuComponent.js
@@ -8,31 +8,8 @@ class MenuComponent extends React.Component {
 
     let dump = require('../../dump/dumpCategories');
 
-    let imgSources = [];
 
-    imgSources.push(require('../../dump/logos/1.png'));
-    imgSources.push(require('../../dump/logos/2.png'));
-    imgSources.push(require('../../dump/logos/3.png'));
-    imgSources.push(require('../../dump/logos/4.png'));
-    imgSources.push(require('../../dump/logos/5.png'));
-    imgSources.push(require('../../dump/logos/6.png'));
-    imgSources.push(require('../../dump/logos/7.png'));
-    imgSources.push(require('../../dump/logos/8.png'));
-    imgSources.push(require('../../dump/logos/9.png'));
-    imgSources.push(require('../../dump/logos/10.png'));
-    imgSources.push(require('../../dump/logos/11.png'));
-    imgSources.push(require('../../dump/logos/12.png'));
-    imgSources.push(require('../../dump/logos/13.png'));
-    imgSources.push(require('../../dump/logos/14.png'));
-    imgSources.push(require('../../dump/logos/15.png'));
-    imgSources.push(require('../../dump/logos/16.png'));
-    imgSources.push(require('../../dump/logos/17.png'));
-    imgSources.push(require('../../dump/logos/18.png'));
-    imgSources.push(require('../../dump/logos/19.png'));
-    imgSources.push(require('../../dump/logos/20.png'));
-    imgSources.push(require('../../dump/logos/21.png'));
-
-
+    let imgSources = require('../../dump/logos');
 
     this.state = {
       menuItems: dump.categories,
@@ -43,9 +20,13 @@ class MenuComponent extends React.Component {
   _pressRow(rowID: number) {
     this.props.navigation.navigate('ListBusinesses', {
       categoryId: 'Lucy'
-    })
+    });
   }
-  
+
+  gotoAbout() {
+    this.props.navigation.navigate('About')
+  }
+
   render() {
     return (
       <View style={ styles.sideMenu }>
@@ -54,13 +35,12 @@ class MenuComponent extends React.Component {
         </View>
         <ScrollView>
         <List containerStyle={ styles.menuContainer }>
+          <ListItem  onPress={ this.gotoAbout.bind(this) } title='Giới thiệu' />
+          <ListItem  onPress={ () => this._pressRow(1) } title='Tin khuyến mãi' />
           { this.state.menuItems.map((l, i) => {
               let imgSource = this.state.images[Math.floor(Math.random()*this.state.images.length)];
-
               return (
-
-                  <ListItem  roundAvatar avatar={ imgSource } onPress={ () => this._pressRow(1) } key={ i } title={ l.name } />
-
+                <ListItem  roundAvatar avatar={ imgSource } onPress={ () => this._pressRow(1) } key={ i } title={ l.name } />
               )
             }) }
         </List>
---
+++ app/dump/logos/index.js
@@ -0,0 +1,25 @@
+let imgSources = [];
+
+imgSources.push(require('./1.png'));
+imgSources.push(require('./2.png'));
+imgSources.push(require('./3.png'));
+imgSources.push(require('./4.png'));
+imgSources.push(require('./5.png'));
+imgSources.push(require('./6.png'));
+imgSources.push(require('./7.png'));
+imgSources.push(require('./8.png'));
+imgSources.push(require('./9.png'));
+imgSources.push(require('./10.png'));
+imgSources.push(require('./11.png'));
+imgSources.push(require('./12.png'));
+imgSources.push(require('./13.png'));
+imgSources.push(require('./14.png'));
+imgSources.push(require('./15.png'));
+imgSources.push(require('./16.png'));
+imgSources.push(require('./17.png'));
+imgSources.push(require('./18.png'));
+imgSources.push(require('./19.png'));
+imgSources.push(require('./20.png'));
+imgSources.push(require('./21.png'));
+
+module.exports = imgSources;
