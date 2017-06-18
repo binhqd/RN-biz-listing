822394929ef528b64fd7cdd6d1469e51f7729f80
binhqd <binhqd@gmail.com>
2017-06-18 10:57:43 +0700

    Use real data for business images in detail page

--- app/components/BusinessDetail/BizImageList.js
+++ app/components/BusinessDetail/BizImageList.js
@@ -11,20 +11,12 @@ class BizImageList extends React.Component {
   constructor(props, context) {
     super(props, context);
 
-    let images = [];
-    images.push(require('./1.jpg'));
-    images.push(require('./2.jpg'));
-    images.push(require('./3.jpg'));
-    images.push(require('./1.jpg'));
-    images.push(require('./2.jpg'));
-    images.push(require('./3.jpg'));
-    images.push(require('./1.jpg'));
-    images.push(require('./2.jpg'));
-
     this.state = {
       images: this.props.images,
       groups: []
     }
+
+    this.cols = 3;
   }
 
   grouping(items, n) {
@@ -38,28 +30,34 @@ class BizImageList extends React.Component {
   }
 
   componentDidMount() {
-    let n = 3;
-    this.grouping(this.state.images, n);
+    this.grouping(this.state.images, this.cols);
   }
 
   render() {
     return (
       <View style={ styles.container}>
-      <Swiper style={styles.wrapper} showsPagination={false}>
-      {
-        this.state.groups.map((group, index) => {
-          return (
-            <View style={ styles.slide } key={index}>
-              {
-                group.map((item, itemIndex) =>
-                  <Image key={itemIndex} style={ styles.bizThumbnail } source={ {uri: `${CONFIG.STATIC_URL}/${item.container}/${item.name}`} } />
-                )
+        <Swiper style={styles.wrapper} showsPagination={false}>
+        {
+          this.state.groups.map((group, index) => {
+            let images = [];
+            for (let i = 0; i < this.cols; i++) {
+              let item = group[i];
+              if (typeof item != "undefined") {
+                images.push(<Image key={i} style={ styles.bizThumbnail } source={ {uri: `${CONFIG.STATIC_URL}/${item.container}/${item.name}`} } />);
+              } else {
+                images.push(<Image key={i} style={ styles.bizThumbnail } source={ {uri: `http://via.placeholder.com/350x150`} } />);
               }
-            </View>
-          )
-        })
-      }
-      </Swiper>
+
+            }
+
+            return (
+              <View style={ styles.slide } key={index}>
+                {images}
+              </View>
+            )
+          })
+        }
+        </Swiper>
       </View>
     )
   }
@@ -70,10 +68,14 @@ let styles = {
     flex: 1,
     backgroundColor: '#ddd',
     height: 75,
-    flexDirection: 'column'
+    flexDirection: 'column',
+    padding: 0,
+    margin: 0
   },
   wrapper: {
-    flex: 1
+    flex: 1,
+    margin: 0,
+    padding: 0
   },
   slide: {
     flex: 1,
@@ -82,7 +84,8 @@ let styles = {
     justifyContent: 'space-around'
   },
   bizThumbnail: {
-    flex: 0.28,
+    // display: 'flex',
+    width: (width / 3)- 5,
     height: 75
   }
 }
